61653c64af649c8bc889471bccd23d19a6565c7b
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-08 22:05:11 +0545

    commit

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -4,28 +4,26 @@ import cn from "classnames";
 const Meme = () => {
   return (
     <section className={MemeCSS.memeGenerator}>
-      <form action="">
-        <div className={MemeCSS.meme__input}>
-          <input type="text" placeholder="Top text" name="setup" id="setup" />
-          <input
-            type="text"
-            placeholder="Bottom text"
-            name="punchline"
-            id="punchline"
+      <div className={MemeCSS.meme__input}>
+        <input type="text" placeholder="Top text" name="setup" id="setup" />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          name="punchline"
+          id="punchline"
+        />
+      </div>
+
+      <button className={MemeCSS.meme__button}>
+        <div className={MemeCSS.meme__container}>
+          <div>Get a new meme image</div>
+          <img
+            src="/images/window.png"
+            alt="window-frame"
+            style={{ width: "25px" }}
           />
         </div>
-
-        <button className={MemeCSS.meme__button}>
-          <div className={MemeCSS.meme__container}>
-            <div>Get a new meme image</div>
-            <img
-              src="/images/window.png"
-              alt="window-frame"
-              style={{ width: "25px" }}
-            />
-          </div>
-        </button>
-      </form>
+      </button>
     </section>
   );
 };
